05c9925777b442ebb9f49e1ca2c5b2508011ff4f
Galia101 <galiya.almejrin@gmail.com>
2023-09-10 04:12:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+class person:
+    #Attributes
+    name = "Galia"
+    age:17
+    def __init__(self, name,age):
+         self.name = name
+         self,age = age
+    def _init_(self):
+         return f"myname is {self.nameb} and I am {self.age} years old"
+    
+    def is_adult(self):
+         if self.age>18:
+
+              print("you have too much responsidilities")
+
+first_person = person("Galia", 17)
+# print(first_person.age,first_person.name)
+class person:
+     else:
+     print("Lucky you")
+first_person.is_adult()
